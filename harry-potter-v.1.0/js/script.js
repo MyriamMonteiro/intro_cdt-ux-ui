8240afbcaf6f72293073b34ba0dcac7a22f43f14
Myriam M. <myriam_job27@hotmail.com>
2025-09-04 14:28:09 -0300

    harry-potter-v.1.0
    Nova versão com as fotos e formatações atualizadas. Separados em pastas Html, Css e Javascript

---
+++ harry-potter-v.1.0/js/script.js
@@ -0,0 +1,84 @@
+/* JS PÁGINA index.html */
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.getElementById('menu-toggle');
+    const navLinks = document.getElementById('nav-links');
+    
+    menuToggle.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+});
+
+/* -------------------------------------------------------------------------------- /*
+
+/* JS PÁGINA sobre.html */
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.getElementById('menu-toggle');
+    const navLinks = document.getElementById('nav-links');
+    
+    menuToggle.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+});
+
+
+/* -------------------------------------------------------------------------------- /*
+
+/* JS PÁGINA fundador.html */
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.getElementById('menu-toggle');
+    const navLinks = document.getElementById('nav-links');
+    
+    menuToggle.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+});
+
+/* -------------------------------------------------------------------------------- /*
+
+/* JS PÁGINA integrantes.html */
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.getElementById('menu-toggle');
+    const navLinks = document.getElementById('nav-links');
+    
+    menuToggle.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+});
+
+/* -------------------------------------------------------------------------------- /*
+
+/* JS PÁGINA draco.html */
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.getElementById('menu-toggle');
+    const navLinks = document.getElementById('nav-links');
+    
+    menuToggle.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+});
+
+
+/* -------------------------------------------------------------------------------- /*
+
+/* JS PÁGINA tom.html */
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.getElementById('menu-toggle');
+    const navLinks = document.getElementById('nav-links');
+    
+    menuToggle.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+});
+
+
+/* -------------------------------------------------------------------------------- /*
+
+/* JS PÁGINA regulus.html */
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.getElementById('menu-toggle');
+    const navLinks = document.getElementById('nav-links');
+    
+    menuToggle.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+});
